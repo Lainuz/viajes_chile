16e5b9c190c7392f6a54311f912255ba51e9126c
Paulino <paulino.msb@gmail.com>
2022-02-05 22:52:45 -0300

    version 1.0

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* Inicia Tooltips en todo el sitio */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+/* Funciones JQuery */
+$(document).ready(function () {
+    var blanco = true;
+
+    /* Cambio de color Cabecera Sección Quienes Somos */
+    $(".header").hover(function () {
+        $(this).css({
+            "background-color": "white",
+            "color": "#259fab"
+        });
+    }, function () {
+        $(this).css({
+            "background-color": "#259fab",
+            "color": "white"
+        });
+    });
+
+});
